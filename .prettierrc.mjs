a9ffb587cca6066942acad920db26a5a2069b6d1
Jason Trib <jasontrib10@gmail.com>
2023-10-14 18:31:55 +0300

    basic setup of eslint and prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,43 @@
+/** @type {import("prettier").Config} */
+
+export default {
+  printWidth: 120,
+  singleQuote: true,
+  overrides: [
+    {
+      files: ['**/overrides/**'],
+      excludeFiles: ['*/overrides/**/exception.tsx'],
+      options: {
+        semi: false,
+        singleQuote: false,
+        trailingComma: 'none',
+      },
+    },
+  ],
+};
+
+/** Default Options */
+/** 
+    arrowParens: 'always',
+    bracketSameLine: false,
+    bracketSpacing: true,
+    embeddedLanguageFormatting: 'auto',
+    endOfLine: 'lf',
+    filepath: undefined,
+    htmlWhitespaceSensitivity: 'css',
+    insertPragma: false,
+    jsxSingleQuote: false,
+    printWidth: 80,
+    proseWrap: 'preserve',
+    quoteProps: 'as-needed',
+    rangeEnd: Infinity,
+    rangeStart: 0,
+    requirePragma: false,
+    semi: true,
+    singleAttributePerLine: false,
+    singleQuote: false,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    useTabs: false,
+    vueIndentScriptAndStyle: false
+*/
